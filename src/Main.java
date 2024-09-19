66795a342774ba20e922aede0e1d1321c7576c9b
Corey Mays <corey.mays17@gmail.com>
2024-09-19 15:24:34 -0400

    save commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,42 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        int intOperandA = 26;
+        int intOperandB = 16;
+        int intSum = 0;
+        int intProduct = 0;
+        int intQuotient = 0;
+        int intDifference = 0;
+        int intModulo = 0;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The remainder of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+
+
+        double doubleOperandA = 14.18;
+        double doubleOperandB = 3.14;
+
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleQuotient = 0;
+        double doubleDifference = 0;
+        double doubleModulo = 0;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println();
+
+        // complete this for the doubles
+
+
+
     }
 }
